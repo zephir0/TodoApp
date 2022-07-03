ea6d0ab1e443eb5ef375a43affb20688597b88b0
= <=>
2022-07-03 01:56:28 +0100

    Added message after logging in

---
+++ src/main/java/com/example/todoapp/message/MessageService.java
@@ -0,0 +1,15 @@
+package com.example.todoapp.message;
+
+import com.example.todoapp.user.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+    public String getWelcomeMessage() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName().toUpperCase();
+        String message = ", WELCOME IN TodoApp";
+        return username + message;
+    }
+}
--- src/main/java/com/example/todoapp/web/HomeController.java
+++ src/main/java/com/example/todoapp/web/HomeController.java
@@ -1,14 +1,22 @@
 package com.example.todoapp.web;
 
+import com.example.todoapp.message.MessageService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    private final MessageService messageService;
+
+    public HomeController(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        String welcomeMessage = messageService.getWelcomeMessage();
+        model.addAttribute("welcomeMessage", welcomeMessage);
         return "home";
     }
 }
